497229e42c4cb3d45867421b3626f0a6d328727d
Nicholas <spammiehere@gmail.com>
2023-09-28 10:38:36 -0400

    Update work page

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,19 +5,19 @@ function Work() {
           <div className="post-content">
               <hr />
               <p className="my-4">You can view some of my <strong>professional</strong> work here </p>
-              <p className="my-4"><a className="underline" href="https://teamaftermath.com/" target="_blank" rel="noreferrer"><strong>AfterMath</strong></a> - Designed + developed the company site</p>
-              <p className="my-4"><a className="underline" href="https://www.bdmorganfdn.org/2018-annual-report/" target="_blank" rel="noreferrer"><strong>BDM Microsite</strong></a> - Developed a microsite for the foundation's annual report.</p>
-              <p className="my-4"><a className="underline" href="https://www.neoship.org/" target="_blank" rel="noreferrer"><strong>SHIP Website</strong></a> - Developed a CMS site for Triad's "Design for Good" campaign.</p>
+              <p className="my-4"><a className="underline" href="https://teamaftermath.com/" target="_blank" rel="noreferrer"><strong>AfterMath</strong></a> - Developed + designed the company site.</p>
               <p className="my-4"><a className="underline" href="https://www.bawls.com/" target="_blank" rel="noreferrer"><strong>Bawls Guarana</strong></a> - Developed the home page + information pages for the gamer drink.</p>
+              <p className="my-4"><a className="underline" href="https://www.bdmorganfdn.org/2018-annual-report/" target="_blank" rel="noreferrer"><strong>BDM Microsite</strong></a> - Developed a microsite for the foundation's annual report.</p>
+              <p className="my-4"><a className="underline" href="https://www.neoship.org/" target="_blank" rel="noreferrer"><strong>SHIP Website</strong></a> - Developed a Drupal CMS site for Triad's "Design for Good" campaign.</p>
               <hr />
           </div>
         
         <h2 className="text-4xl font-bold my-4">Personal Work</h2>
           <div className="post-content">
                 <hr />
-                <p className="my-4">These are <strong>Under Construction</strong>, let me know if you want to help!</p>
+                <p className="my-4">These are <strong>Under Construction</strong>, wear your hard hats!</p>
                 <p className="my-4"><a className="underline" href="https://helpher.io/" target="_blank" rel="noreferrer"><strong>HelpHer.io</strong></a> - I'm working on a helpful resource for women.</p>
-                <p className="my-4"><a className="underline" href="https://main--tubular-boba-955693.netlify.app/" target="_blank" rel="noreferrer"><strong>National Park Info</strong></a> - I made this little page to show some park data.</p>
+                <p className="my-4"><a className="underline" href="https://main--tubular-boba-955693.netlify.app/" target="_blank" rel="noreferrer"><strong>National Park Info</strong></a> - This little page pulls some useful info from the National Park API.</p>
                 <p className="my-4"><a className="underline" href="https://github.com/Nerptastic/EZCSS" target="_blank" rel="noreferrer"><strong>EZCSS</strong></a> - I use this CSS for spinning up smaller sites.</p>
                 <hr />
           </div>
