fc020b29c95b260c39321474ca612dd4307c84a6
Nerptastic <spammiehere@gmail.com>
2024-02-03 12:33:41 -0500

    Update resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,16 +10,16 @@ function Resume() {
           <p className="my-2"><em>BFA, Graphic Design,</em> <strong>The University of Akron</strong>, 2021</p>
           <hr />
           <h4 className="my-2 text-xl"><strong>Work Experience</strong></h4>
-          <p className="my-4">Web Developer, <strong>Cohen & Company</strong> (<em>current</em>)</p>
-          <p className="my-4">Web Developer / Designer, <strong>AfterMath, Inc.</strong> (<em>2022</em>)</p>
-          <p className="my-4">Web Developer Intern, <strong>Triad Advertising</strong> (<em>2021</em>)</p>
-          <p className="my-4">Web Developer, <strong>Yoder Graphics Systems, Inc.</strong> (<em>2019</em>)</p>
+          <p className="my-4">Web Developer (Contract), <strong>Columbia University</strong> (<em>January 2024–Present</em>)</p>
+          <p className="my-4">Web Developer, <strong>Cohen & Company</strong> (<em>May 2022–October 2023</em>)</p>
+          <p className="my-4">Jr. Web Developer / Designer, <strong>AfterMath, Inc.</strong> (<em>May 2021–May 2022</em>)</p>
+          <p className="my-4">Web Developer Intern, <strong>Yoder Graphics Systems, Inc.</strong> (<em>December 2019–May 2021</em>)</p>
           <hr />
           <h4 className="my-2 text-xl"><strong>Technologies</strong></h4>
-          <p className="my-4"><em>Technologies:</em> React, JavaScript, HTML, CSS, TypeScript, Tailwind, SCSS, Bootstrap, Node.js, PHP, <br /> SQL,                 
-                         Git, npm, WordPress, Drupal, jQuery, Webpack, ESLint</p>
-          <p className="my-4"><em>Skills:</em> Agile, Technical Writing, UI/UX Design, Graphic Design, Copywriting, Video Editing</p>
-          <p className="my-4"><em>Interests:</em> Home Servers, Guitar, Piano, Motorcycles, Powerlifting, Tattoos, Reading</p>
+          <p className="my-4"><em>Technologies:</em> React, Next.js, JavaScript (ES6+), TypeScript, HTML5, CSS3, Node.js, Express.js, MongoDB,<br />
+                                                      SQL, Git, Webpack, Jest, ESLint, RESTful APIs, Adobe Creative Suite, Figma</p>
+          <p className="my-4"><em>Skills:</em> Technical Writing, CI/CD, Agile Methodologies, SEO, UI/UX Design, Wireframing, Prototyping</p>
+          <p className="my-4"><em>Interests:</em> Home Servers, Raspberry Pi, Guitar, Piano, Motorcycles, Powerlifting, Tattoos, Reading</p>
         </div>
         <hr />
         <p className="text-right"><span><em>design, learn, refine</em></span></p>
